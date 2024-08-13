58eb261c272bbd691097b8433c326e3e3f73c27a
Rasa <rasa.zukauskaite@gmail.com>
2024-08-13 10:24:21 +0300

    first design

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ function App() {
   
   return (
     <main>
-      <h1>Gyvybės</h1>
+      <div className="header">
+        <h1>Žaidimas kartuvės</h1>
+      </div>
+      <div className="stats">
+        <h2>Likusios gyvybės:</h2>
+        <h2>Laimėjimai:</h2>
+      </div>
+      <div className="picture">
+        <img src="" alt="kartuves" />
+      </div>
+      <div>
+        <p>Keyboard</p>
+      </div>
     </main>
     
     
